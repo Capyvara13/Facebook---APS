d78fe2e5f88b22f88d3c164a4faee67dd67ad2e5
Capyvara13 <Tiagorafleite@gmial.com>
2024-06-12 15:31:34 -0300

    Login

--- menu/MenuScript.js
+++ menu/MenuScript.js
@@ -1,13 +1,22 @@
-var menu = document.getElementById("menu")
-var botão1 = document.getElementById("button1")
+var menu = document.getElementById("menu");
+var botão1 = document.getElementById("button1");
+var text1 = document.getElementById("textbox1");
+var timer; // Definir uma variável de temporizador global
 
 botão1.addEventListener('mouseover', function() {
     menu.classList.add("BarraVai");
-    menu.classList.remove("barraParaTrás");
-    menu.style.width = "22vw"
+    menu.classList.remove("BarraVolta");
+    menu.style.width = "22vw";
+    text1.classList.add("TextLeft");
+    text1.classList.remove("textRight");
+    text1.style.left = "5.5vw";
 });
+
 botão1.addEventListener('mouseout', function() {
-    menu.classList.add("barraParaTrás");
+    menu.classList.add("BarraVolta");
     menu.classList.remove("BarraVai");
-    menu.style.width = "14vw"
+    menu.style.width = "14vw";
+    text1.classList.add("textRight");
+    text1.classList.remove("textLeft");
+    text1.style.left = "23vw";
 });
