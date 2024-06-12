82856024d9eecda6f6f1477c63f006ba338a4b57
Capyvara13 <Tiagorafleite@gmial.com>
2024-06-12 16:00:14 -0300

    Atualização menu

--- menu/MenuScript.js
+++ menu/MenuScript.js
@@ -1,7 +1,7 @@
 var menu = document.getElementById("menu");
 var botão1 = document.getElementById("button1");
-var text1 = document.getElementById("textbox1");
-var timer; // Definir uma variável de temporizador global
+var text1 = document.getElementById("button-p1");
+var textBox1 = document.getElementById("textbox1")
 
 botão1.addEventListener('mouseover', function() {
     menu.classList.add("BarraVai");
@@ -9,7 +9,10 @@ botão1.addEventListener('mouseover', function() {
     menu.style.width = "22vw";
     text1.classList.add("TextLeft");
     text1.classList.remove("textRight");
-    text1.style.left = "5.5vw";
+    text1.style.left = "0vw";
+    textBox1.classList.add("overflowOn")
+    textBox1.classList.remove("overflowOff")
+    textBox1.style.width = "16.5vw"
 });
 
 botão1.addEventListener('mouseout', function() {
@@ -19,4 +22,7 @@ botão1.addEventListener('mouseout', function() {
     text1.classList.add("textRight");
     text1.classList.remove("textLeft");
     text1.style.left = "23vw";
+    textBox1.classList.add("overflowOff")
+    textBox1.classList.remove("overflowOn")
+    textBox1.style.width = "8.5vw"
 });
